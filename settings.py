a0da77a40a41bd4102e7c663343233a0b2d54164
Damilola <damilolaabiolasa@gmail.com>
2025-10-09 12:33:15 +0100

    module contains settings for ship and entire game

---
+++ settings.py
@@ -0,0 +1,14 @@
+class Settings():
+    """A class to store all settings for Alien Invasion"""
+
+    def __init__(self):
+        """Initializations of game settings"""
+        
+        #ship setting
+        self.ship_speed_factor = 1.5
+
+        #Screen settings
+        self.screen_width= 1200
+        self.screen_height= 800
+        self.bg_colour= (230,230,230)
+        
