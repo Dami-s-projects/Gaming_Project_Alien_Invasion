31883891481f5f05b7f0d29783e1f6439e1d0542
Damilola <damilolaabiolasa@gmail.com>
2025-10-14 17:04:35 +0100

    Added new attributes to settings class
    These attrubutes will be the settings for the bullets fired On
    the screen.

--- settings.py
+++ settings.py
@@ -11,4 +11,11 @@ class Settings():
         self.screen_width= 1200
         self.screen_height= 800
         self.bg_colour= (230,230,230)
+
+        #Bullet settings
+        self.bullet_speed_factor = 1
+        self.bullet_width = 3
+        self.bullet_height = 15
+        self.bullet_colour = 60, 60, 60
+
         
