61ba12ae5f8337c4230baa49e53ac1e00cab84ed
Damilola <damilolaabiolasa@gmail.com>
2025-10-18 10:01:52 +0100

    Modify update_screen function
    Update the function by putting a "alien" parameter to its arguement
    as well as adding a corresponding alien.blit_me() method that calls
    the blit_me() method in the alien class.
    
    The purpose of this change is to enable me to update the screen with
    the alien image when called in the main game module.

--- game_functions.py
+++ game_functions.py
@@ -59,7 +59,7 @@ def check_events(ship_1,bullet_setting,screen,bullets):
                  
                  
 
-def update_screen(screen,ship_1,screen_setting,bullets):
+def update_screen(screen,ship_1,screen_setting,bullets,alien):
     """A function that contains code that displays ship and 
     shows the latest screen(frame) """
      #Redraw the screen each pass through the loop with the 
@@ -72,6 +72,7 @@ def update_screen(screen,ship_1,screen_setting,bullets):
         bullet.draw_bullet()
     #Display ship
     ship_1.blit_me()
+    alien.blit_me()
     
     #code that makes the most recently drawn screen (scene) visible
     pygame.display.flip()
