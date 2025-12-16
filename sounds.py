7d58f9effaa10e83df7b34124e1c25ca8c87dc13
Damilola <damilolaabiolasa@gmail.com>
2025-12-16 14:56:32 +0000

    Restructured the sound class and addded new method
    I ensured that self.sound was made to be an attribute in the init
    method of the class, as this would help me use it repeatedly in
    the other methods of the class. Created a new method that reduces
    the sound volume. I added this new method because the explosion
    sound file i downloaded is too loud so this method will reduce the
    volume.

--- sounds.py
+++ sounds.py
@@ -7,9 +7,13 @@ class Sound():
     """play sounds"""
     def __init__(self,filename):
         self.filename = filename
+        self.sound = pygame.mixer.Sound(self.filename)
 
     
     def play_shooting_sound(self):
         """Plays the shooting sound"""
-        sound = pygame.mixer.Sound(self.filename)
-        sound.play()
+        self.sound.play()
+    
+    def decrease_sound_volume(self):
+        """Reduces the sound volume, makes it a bit lower (20 percent)"""
+        self.sound.set_volume(0.2)
