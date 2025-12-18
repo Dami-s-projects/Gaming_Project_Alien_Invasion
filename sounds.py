db812415a66535c5eb8e4b65c267868929a9039b
Damilola <damilolaabiolasa@gmail.com>
2025-12-18 01:50:57 +0000

    Added a class to the sounds module
    Added a class that has methods that can play the background music
    and also stop the backgound music.

--- sounds.py
+++ sounds.py
@@ -17,3 +17,17 @@ class Sound():
     def decrease_sound_volume(self):
         """Reduces the sound volume, makes it a bit lower (20 percent)"""
         self.sound.set_volume(0.2)
+
+class BackgroundMusic(Sound):
+    def __init__(self, filename):
+        super().__init__(filename)
+        self.filename = filename
+        pygame.mixer.music.load(filename)
+
+    def play_music(self):
+        """Plays background music"""
+        pygame.mixer.music.play(-1)
+
+    def stop_music(self):
+        """Stop background music"""
+        pygame.mixer.music.stop()
